cc5e136d2f8ddddf8d5ddd55c72155ad6fedd2fa
aachitty <aachittilappilly@gmail.com>
2024-08-09 10:27:41 +0530

    Updated code to narrow search with synopsis

--- Scrapy/spiders/updated_movie_spider.py
+++ Scrapy/spiders/updated_movie_spider.py
@@ -20,18 +20,20 @@ class MovieSpider(CrawlSpider):
     }
 
 def parse_item(self, response):
-        sel = Selector(response)
+        self = Selector(response)
         movie_title = "Paradise"  # Movie to search for
+        movie_synopsis = "An Indian tourist couple arrive in the hill country of crisis ridden Sri Lanka to celebrate their 5th wedding anniversary. But, when things take an unexpected turn, conflicts deepen revealing cracks in their relationship."
         
         # Check if the URL is in the /movie section
         if '/movie/' in response.url:
-            title_elements = sel.xpath('//h1/text() | //h2/text() | //p/text()').extract()  # Search in h1, h2, and p elements
+            title_elements = self.xpath('//h1/text() | //h2/text() | //p/text()').extract()  # Search in h1, h2, and p elements
             
             for title in title_elements:
-                # Check if the extracted title has a maximum of three words and contains the movie title
-                if len(title.split()) <= 3 and movie_title.lower() in title.lower():
+                # Check if the extracted title contains the movie title and synopsis
+                if movie_title.lower() in title.lower() and movie_synopsis.lower() in title.lower():
+                    
                     output = f"Found {movie_title} in {response.url}\n"
-                    with open('movies_found.txt', 'a') as f:
+                    with open('updated_movies_found.txt', 'a') as f:
                         f.write(output)
                     yield {
                         'url': response.url,
